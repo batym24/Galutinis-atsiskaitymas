d6f9d7e75e64715d61bf8cd767ecf7f689262919
batym24 <baty.mantas@gmail.com>
2023-05-16 15:35:01 +0300

    Created component for registration form and imported styled-components to it for styling

---
+++ src/components/Registration.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+    
+`
+
+const Registration = () => {
+    return ( 
+        <StyledMain>
+        </StyledMain>
+     );
+}
+ 
+export default Registration;
