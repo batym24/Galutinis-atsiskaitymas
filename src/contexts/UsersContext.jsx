3326a0b85b02287b269ff007924202ec31e931a7
batym24 <baty.mantas@gmail.com>
2023-05-17 09:58:27 +0300

    Wrote functionality to get all the users from local server using useReducer hook

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -1,8 +1,22 @@
-import { createContext } from "react";
+import { createContext, useReducer, useEffect, useState } from "react";
 
 const UsersContext = createContext()
 
+const ACTION_TYPE = {
+    GET: 'getAllUsers'
+}
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case ACTION_TYPE.GET:
+            return action.data
+    }
+}
+
 const UsersProvider = ({children}) => {
+
+    const [users, setUsers] = useReducer(reducer, [])
+
     return ( 
         <UsersContext.Provider
             value={{
