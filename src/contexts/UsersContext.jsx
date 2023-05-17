0316a81a5f166b51263c20bd96b7a29a32d1cf4e
batym24 <baty.mantas@gmail.com>
2023-05-17 10:01:55 +0300

    Using useEffect hook, retrieved the user data from local server and set it as state

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -17,6 +17,17 @@ const UsersProvider = ({children}) => {
 
     const [users, setUsers] = useReducer(reducer, [])
 
+    useEffect(() => {
+        fetch('http://localhost:8080/user')
+        .then(res => res.json())
+        .then(data => setUsers({
+            type: ACTION_TYPE.GET,
+            data: data
+        }))
+    }, [])
+
+    console.log(users)
+
     return ( 
         <UsersContext.Provider
             value={{
