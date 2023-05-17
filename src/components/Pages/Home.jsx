22d81553f174111e736224a1af38e9f08abb26a3
batym24 <baty.mantas@gmail.com>
2023-05-17 13:50:43 +0300

    Created Home page component

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledMain = styled.main`
+    
+`
+
+const Home = () => {
+    return ( 
+        <StyledMain>
+        </StyledMain>
+     );
+}
+ 
+export default Home;
