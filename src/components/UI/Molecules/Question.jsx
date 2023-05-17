f5d2349aa5d9516785acdda57206e203069aed77
batym24 <baty.mantas@gmail.com>
2023-05-17 14:48:35 +0300

    Using useEffect hook, retrieved the questions data from local server and set it as state

---
+++ src/components/UI/Molecules/Question.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+    .votes-answers{
+        
+    }
+`
+
+const Question = () => {
+    return ( 
+        <StyledDiv>
+            <div className="votes-answers"></div>
+        </StyledDiv>
+     );
+}
+ 
+export default Question;
