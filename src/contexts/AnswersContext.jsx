ce0370d53802480ee94dc3ab1815e203f140323f
batym24 <baty.mantas@gmail.com>
2023-05-18 14:16:24 +0300

    Created reducer function in AnswersContext and with it retrieved data from local server

--- src/contexts/AnswersContext.jsx
+++ src/contexts/AnswersContext.jsx
@@ -1,12 +1,39 @@
-import { createContext } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const AnswersContext = createContext()
 
+const ANSWERS_ACTION_TYPE = {
+    GET: 'getAllAnswers'
+}
+
+const reducer = (state,action) => {
+    switch(action.type){
+        case ANSWERS_ACTION_TYPE.GET:
+            return action.data
+    }
+}
+
 const AnswersProvider = ({children}) => {
+
+    const [answers, setAnswers] = useReducer(reducer, [])
+
+    useEffect(() => {
+        fetch(`http://localhost:8080/answers`)
+        .then(res => res.json())
+        .then(data => setAnswers({
+            type: ANSWERS_ACTION_TYPE.GET,
+            data: data
+        }))
+    },[])
+
+    console.log(answers)
+
     return ( 
         <AnswersContext.Provider
             value={{
-                
+                ANSWERS_ACTION_TYPE,
+                answers,
+                setAnswers
             }}
         >
             {children}
