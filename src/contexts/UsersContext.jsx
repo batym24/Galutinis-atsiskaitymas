96879e6f65c52358a3b78fdbac37c1fdfbb3ae6a
batym24 <baty.mantas@gmail.com>
2023-05-23 11:00:58 +0300

    Updated registration functionality

--- src/contexts/UsersContext.jsx
+++ src/contexts/UsersContext.jsx
@@ -3,7 +3,8 @@ import { createContext, useReducer, useEffect, useState } from "react";
 const UsersContext = createContext()
 
 const ACTION_TYPE = {
-    GET: 'getAllUsers'
+    GET: 'getAllUsers',
+    ADD: 'addNewUser'
 }
 
 const reducer = (state, action) => {
@@ -37,8 +38,6 @@ const UsersProvider = ({children}) => {
             data: data
         }))
     }, [])
-
-    console.log(users)
 
     return ( 
         <UsersContext.Provider
